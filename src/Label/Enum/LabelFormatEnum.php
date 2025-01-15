7809549cfb87645f415a180e18bf95830d8b66ed
Patrik Picka <patrik.picka@4camping.cz>
2025-01-15 17:48:40 +0100

    add-a6-on-a6-label-format

--- src/Label/Enum/LabelFormatEnum.php
+++ src/Label/Enum/LabelFormatEnum.php
@@ -6,6 +6,7 @@ namespace PTB\PacketaApi\Label\Enum;
 
 enum LabelFormatEnum: string
 {
+	case A6_ON_A6 = 'A6 on A6';
 	case A6_ON_A4 = 'A6 on A4';
 	case A7_ON_A4 = 'A7 on A4';
 	case A8_ON_A4 = 'A8 on A4';
@@ -13,6 +14,7 @@ enum LabelFormatEnum: string
 	public function getMaxOffset(): int
 	{
 		return match ($this) {
+			self::A6_ON_A6 => 0,
 			self::A6_ON_A4 => 3,
 			self::A7_ON_A4 => 7,
 			self::A8_ON_A4 => 15,
